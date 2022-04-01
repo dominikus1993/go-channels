67b1c1c994c2890980370a6b4635b098464bcb59
Dominik.Kotecki <dominikus1910@gmail.com>
2022-04-01 17:47:13 +0000

    add filter context

--- filter.go
+++ filter.go
@@ -2,7 +2,7 @@ package channels
 
 import "context"
 
-func Filter[T any](ctx context.Context, source Stream[T], predicate func(ctx context.Context, element T) bool) Stream[T] {
+func FilterContext[T any](ctx context.Context, source Stream[T], predicate func(ctx context.Context, element T) bool) Stream[T] {
 	out := make(chan T)
 	go func() {
 		defer close(out)
@@ -17,6 +17,19 @@ func Filter[T any](ctx context.Context, source Stream[T], predicate func(ctx con
 				if predicate(ctx, element) {
 					out <- element
 				}
+			}
+		}
+	}()
+	return Stream[T]{channel: out}
+}
+
+func Filter[T any](source Stream[T], predicate func(element T) bool) Stream[T] {
+	out := make(chan T)
+	go func() {
+		defer close(out)
+		for v := range source.channel {
+			if predicate(v) {
+				out <- v
 			}
 		}
 	}()
--- filter_test.go
+++ filter_test.go
@@ -9,16 +9,31 @@ import (
 
 func TestFilter(t *testing.T) {
 	numbers := FromSlice(rangeInt(1, 10))
-	result := Filter(context.TODO(), numbers, func(ctx context.Context, element int) bool { return element%2 == 0 })
+	result := Filter(numbers, func(element int) bool { return element%2 == 0 })
 	subject := ToSlice(result)
 	assert.Len(t, subject, 4)
 	assert.ElementsMatch(t, []int{2, 4, 6, 8}, subject)
 }
 
 func BenchmarkFilter(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		numbers := FromSlice(rangeInt(1, 10))
+		ToSlice(Filter(numbers, func(element int) bool { return element%2 == 0 }))
+	}
+}
+
+func TestFilterContext(t *testing.T) {
+	numbers := FromSlice(rangeInt(1, 10))
+	result := FilterContext(context.TODO(), numbers, func(ctx context.Context, element int) bool { return element%2 == 0 })
+	subject := ToSlice(result)
+	assert.Len(t, subject, 4)
+	assert.ElementsMatch(t, []int{2, 4, 6, 8}, subject)
+}
+
+func BenchmarkFilterContext(b *testing.B) {
 	ctx := context.TODO()
 	for n := 0; n < b.N; n++ {
 		numbers := FromSlice(rangeInt(1, 10))
-		ToSlice(Filter(ctx, numbers, func(ctx context.Context, element int) bool { return element%2 == 0 }))
+		ToSlice(FilterContext(ctx, numbers, func(ctx context.Context, element int) bool { return element%2 == 0 }))
 	}
 }
